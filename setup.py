08f9ae1c440d34453aa11e2b87f53f0038ae0519
Vinícius Figueiredo <vinoba_@hotmail.com>
2024-02-07 01:56:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ description = "Google Generative AI High level API client library and tools."
 
 
 
-version = '1.0.4'
+version = '1.0.5'
 
 dependencies = [
     "google-ai-generativelanguage==0.4.0",
